a7deb925a77cfb5fdbabeb50ab2915368dd2ee50
Sandoche Adittane <sandoche@protonmail.com>
2019-04-07 00:23:15 +0200

    :recycle: Code refactored

--- lib/idea_item.dart
+++ lib/idea_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'styles.dart';
 
 class IdeaItem extends StatelessWidget {
   const IdeaItem({
@@ -19,25 +20,19 @@ class IdeaItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(28.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
             title,
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.w900
-            )
+            style: STYLE_TITLES
           ),
           SizedBox(height: 10),
           Text(
             description,
             maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: const Color(0xff65638f)
-            )
+            overflow: TextOverflow.ellipsis
           ),
           SizedBox(height: 10),
           Row (
---
+++ lib/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const STYLE_TITLES = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.w900
+);
